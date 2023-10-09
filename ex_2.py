3134eb0a8e320e8d84ae41d491c0641efcc3fd0e
kwenus <kwenus82@gmail.com>
2023-10-10 01:39:06 +0300

    почти решил

--- ex_2.py
+++ ex_2.py
@@ -4,13 +4,29 @@ from hw_2_ex_1 import cook_book
 
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
+    names = set()
+    temp_dict = {}
     for dish in dishes:
         for item in cook_book[dish]:
-            shop_list[item['ingredient_name']] = {
-                'quantity': item['quantity'] * person_count,
-                'measure': item['measure']
-            }
+            for key, value in dish.items():
+                products.append([key, value])
+            if item['ingredient_name'] in shop_list:
+
+
+            else:
+                shop_list[item['ingredient_name']] = {
+                    'quantity': [item['quantity']],
+                    'measure': item['measure'],
+                }
+
+
     return shop_list
 
-pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Уха'], 3))
+pprint(get_shop_list_by_dishes(['Нагетсы', 'Крылышки барбекю'], 3))
 
+# if item['ingredient_name'] in shop_list:
+#     shop_list[item['ingredient_name']] = {
+#         'quantity': item['quantity'] + (item['quantity'] * person_count),
+#         'measure': item['measure']
+#     }
+# else:
